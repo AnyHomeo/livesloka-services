111d4b4a8ca0692bc00f550d67b83009fe4ba097
Ronin <iamraviprakash1.com>
2020-10-27 19:54:46 +0530

    env

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/admin');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/admin");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
+// require('./config/config');
+require("dotenv").config();
+require("./models/db");
+require("./config/passportConfig");
 
-require('./config/config');
-require('./models/db');
-require('./config/passportConfig');
+const server = require("http").createServer(app);
 
-
-const server = require('http').createServer(app);
-
-
-server.listen(process.env.PORT || '5000', () =>
+server.listen(process.env.PORT || "5000", () =>
   console.log(`Server started at port : ${process.env.PORT}`)
 );
-
-
 
 module.exports = app;
 //madde for testing
