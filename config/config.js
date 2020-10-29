0f9ad530e4ba98263065cbfa292c8f384efbeae1
charankurru <61787496+charankurru@users.noreply.github.com>
2020-10-29 12:14:14 +0530

    url chnaged

--- config/config.js
+++ config/config.js
@@ -5,3 +5,6 @@ var config = require('./config.json');
 var envConfig = config[env];
 // add env. config values to process.env
 Object.keys(envConfig).forEach((key) => (process.env[key] = envConfig[key]));
+
+
+// "MONGODB_URI": "mongodb+srv://liveslokaDev:8522p@cluster0.ox8do.mongodb.net/<Livesloka>?retryWrites=true&w=majority",
