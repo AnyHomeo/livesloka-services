1c49b0e67c899dfad79d188280c6918513425307
E.Karthik <kartheekenumarthi@gmail.com>
2020-11-12 12:32:45 +0530

    check push

--- controllers/Customer.controller.js
+++ controllers/Customer.controller.js
@@ -1,50 +1,54 @@
-const CustomerModel = require('../models/Customer.model');
+const CustomerModel = require("../models/Customer.model");
 
 module.exports = {
-    async registerCustomer(req, res) {
+  async registerCustomer(req, res) {
+    let customerRegData = new CustomerModel(req.body);
 
-        let customerRegData = new CustomerModel(req.body);
+    customerRegData
+      .save()
+      .then((val) => {
+        res.status(200).send({
+          status: "OK",
+          message: "Customer data inserted Successfully",
+          result: null,
+        });
+      })
+      .catch((err) => {
+        res.status(400).send({
+          status: "Bad Request",
+          message: "Invalid Syntax",
+          result: null,
+        });
+      });
+  },
 
-        customerRegData
-            .save()
-            .then((val) => {
-                res.status(200).send({
-                    status: "OK",
-                    message: "Customer data inserted Successfully",
-                    result: null,
-                });
-            })
-            .catch((err) => {
-                res.status(400).send({
-                    status: "Bad Request",
-                    message: "Invalid Syntax",
-                    result: null,
-                })
-            });
+  async details(req, res) {
+    console.log(req.body);
+    CustomerModel.find({})
+      .select(" -customerId ")
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Customer data retrieved", status: "OK", result });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "something went Wrong", err });
+      });
+  },
 
-    },
-
-    async details(req, res) {
-        console.log(req.body)
-        CustomerModel.find({})
-            .select(" -customerId ")
-            .then(result => { res.status(200).json({ message: "Customer data retrieved", status: "OK", result }) })
-            .catch(err => { res.status(400).json({ message: "something went Wrong", err }); })
-    },
-
-    async updateCustomer(req, res) {
-        console.log(req.body)
-        CustomerModel.update({ _id: req.body._id }, req.body)
-            .then((result) => {
-                res.status(200).send({
-                    status: "OK",
-                    message: "Customer data updated Successfully",
-                    result: null,
-                });
-            })
-            .catch((err) => {
-                res.sendStatus(500);
-                console.log(err);
-            });
-    },
+  async updateCustomer(req, res) {
+    console.log(req.body);
+    CustomerModel.update({ _id: req.body._id }, req.body)
+      .then((result) => {
+        res.status(200).send({
+          status: "OK",
+          message: "Customer data updated Successfully",
+          result: null,
+        });
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+        console.log(err);
+      });
+  },
 };
