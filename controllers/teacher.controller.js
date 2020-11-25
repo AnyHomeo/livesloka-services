c5b3b004726bc790cb722e399d647757443ad8aa
E.Karthik <kartheekenumarthi@gmail.com>
2020-11-25 15:02:51 +0530

    fix: no day in response

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -1,15 +1,14 @@
 const TeacherModel = require("../models/Teacher.model");
-
+const days = [
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+  "SUNDAY",
+];
 exports.validateSlot = (req, res, next) => {
-  let days = [
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-    "SATURDAY",
-    "SUNDAY",
-  ];
   if (!req.body.slot) {
     return res.status(400).json({ stage: 1, error: "Invalid Entry" });
   } else {
@@ -29,6 +28,13 @@ exports.validateSlot = (req, res, next) => {
     ) {
       return res.status(400).json({ stage: 4, error: "Invalid Entry" });
     } else {
+      let { slot } = req.body;
+      req.body.slot =
+        slot.split("-")[0].trim() +
+        "-" +
+        slot.split("-")[1].trim() +
+        "-" +
+        slot.split("-")[2].trim();
       next();
     }
   }
@@ -78,40 +84,47 @@ exports.addSlot = (req, res) => {
 exports.getAvailableSlots = (req, res) => {
   const { day } = req.query;
   const { id } = req.params;
-  TeacherModel.findOne({ id })
-    .select("availableSlots")
-    .then((data) => {
-      let arr = [];
-      if (data.availableSlots) {
-        data.availableSlots.forEach((slot) => {
-          if (slot.startsWith(day)) {
-            arr.push(slot);
-          }
-        });
-        return res.status(200).json({
-          message: "slots retrieved successfully",
-          result: arr,
-        });
-      } else {
-        data.availableSlots = [];
-        data.save((err, docs) => {
-          if (err) {
-            return res.status(500).json({
-              error: "internal server error",
-            });
-          }
-          return res.status(200).json({
-            message: "Slots retrieved successfully",
-            result: [],
+  if (days.includes(day)) {
+    TeacherModel.findOne({ id })
+      .select("availableSlots")
+      .then((data) => {
+        let arr = [];
+        if (data.availableSlots) {
+          data.availableSlots.forEach((slot) => {
+            if (slot.startsWith(day)) {
+              let slotArr = slot.split("-");
+              arr.push(slotArr[1] + "-" + slotArr[2]);
+            }
           });
+          return res.status(200).json({
+            message: "slots retrieved successfully",
+            result: arr,
+          });
+        } else {
+          data.availableSlots = [];
+          data.save((err, docs) => {
+            if (err) {
+              return res.status(500).json({
+                error: "internal server error",
+              });
+            }
+            return res.status(200).json({
+              message: "Slots retrieved successfully",
+              result: [],
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          error: "error in retrieving data",
         });
-      }
-    })
-    .catch((err) => {
-      return res.status(400).json({
-        error: "error in retrieving data",
       });
+  } else {
+    return res.status(400).json({
+      error: "invalid Day!",
     });
+  }
 };
 
 exports.getTeachers = (req, res) => {
