26ccaab2e54e0a26cf48bbaea2a997065067f03c
E.Karthik <kartheekenumarthi@gmail.com>
2020-12-03 19:53:10 +0530

    fix: config file

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 // check env.
-var env = "production";
-// var env = "development";
+// var env = "production";
+var env = "development";
 // fetch env. config
 var config = require("./config.json");
 var envConfig = config[env];
 // add env. config values to process.env
 Object.keys(envConfig).forEach((key) => (process.env[key] = envConfig[key]));
-
-// "MONGODB_URI": "mongodb+srv://liveslokaDev:8522p@cluster0.ox8do.mongodb.net/<Livesloka>?retryWrites=true&w=majority",
