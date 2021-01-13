642277524d9edc3245350f0a7454a7a0f9be4e2d
charankurru <61787496+charankurru@users.noreply.github.com>
2021-01-13 20:22:08 +0530

    nothing

--- controllers/schedule.controller.js
+++ controllers/schedule.controller.js
@@ -90,9 +90,8 @@ const slotPreproccesor = (sluts) => {
       let mintime = arrayMin(evalstamps);
       let maxtime = arrayMax(evalstamps);
       let convertedStamps = covertIntToTimes([mintime, maxtime]);
-      let finalStr = `${slot.toUpperCase()}-${convertedStamps[0]}-${
-        convertedStamps[1]
-      }`;
+      let finalStr = `${slot.toUpperCase()}-${convertedStamps[0]}-${convertedStamps[1]
+        }`;
       sluts[slot] = [finalStr];
       //console.log(slots[slot]);
     }
@@ -316,7 +315,7 @@ const postProcess = (data, cn) => {
   let schdarr = [];
   let schd = "";
   console.log("from postprocess", data);
-  for (q = 0; q < data.length; ) {
+  for (q = 0; q < data.length;) {
     schd = data[q] + "to " + data[q + 1].slice(-9);
     q = q + 2;
     schdarr.push(schd);
@@ -346,7 +345,7 @@ exports.addSchedule = async (req, res) => {
     friday,
     saturday,
     sunday,
-    //meetingLink,
+    meetingLink,
     meetingAccount,
     teacher,
     students,
@@ -376,19 +375,17 @@ exports.addSchedule = async (req, res) => {
   saturday = saturday ? saturday : [];
   sunday = sunday ? sunday : [];
   let className = "";
-  // meetingLink = meetingLink.startsWith("http")
-  //   ? meetingLink
-  //   : "https://" + meetingLink;
-  let meetingLink = "http:// + meetingLink +zoom";
+  meetingLink = meetingLink.startsWith("http")
+    ? meetingLink
+    : "https://" + meetingLink;
   try {
     let selectedSubject = await Subject.findOne({ _id: subject }).lean();
     let selectedTeacher = await Teacher.findOne({ id: teacher }).lean();
     if (classname) {
       className = classname;
     } else {
-      className = `${selectedSubject.subjectName} ${
-        selectedTeacher.TeacherName
-      } ${startDate} ${demo ? "Demo" : ""}`;
+      className = `${selectedSubject.subjectName} ${selectedTeacher.TeacherName
+        } ${startDate} ${demo ? "Demo" : ""}`;
     }
   } catch (error) {
     console.log(error);
@@ -545,7 +542,11 @@ exports.editSchedule = async (req, res) => {
     subject,
     className,
     meetingAccount,
+    OneToOne,
+    oneToMany
   } = req.body;
+
+  console.log(req.body)
 
   let {
     monday,
@@ -573,9 +574,8 @@ exports.editSchedule = async (req, res) => {
     if (className) {
       req.body.className = className;
     } else {
-      req.body.className = `${selectedSubject.subjectName} ${
-        selectedTeacher.TeacherName
-      } ${startDate} ${demo ? "Demo" : ""}`;
+      req.body.className = `${selectedSubject.subjectName} ${selectedTeacher.TeacherName
+        } ${startDate} ${demo ? "Demo" : ""}`;
     }
   } catch (error) {
     console.log(error);
@@ -659,8 +659,7 @@ exports.editSchedule = async (req, res) => {
           meetingLink.split("/")[4].split("?")[0]
         );
         fetch(
-          `https://api.zoom.us/v2/meetings/${
-            meetingLink.split("/")[4].split("?")[0]
+          `https://api.zoom.us/v2/meetings/${meetingLink.split("/")[4].split("?")[0]
           }`,
           {
             method: "DELETE",
@@ -855,8 +854,7 @@ exports.deleteScheduleById = async (req, res) => {
     const meetingAccountData = await ZoomAccountModel.findById(meetingAccount);
 
     fetch(
-      `https://api.zoom.us/v2/meetings/${
-        meetingLink.split("/")[4].split("?")[0]
+      `https://api.zoom.us/v2/meetings/${meetingLink.split("/")[4].split("?")[0]
       }`,
       {
         method: "DELETE",
