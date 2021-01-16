caa3df64857d84e1872db72cdfc7bf077ab12ff6
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2021-01-16 13:12:03 +0530

    again change

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -235,7 +235,9 @@ exports.getDailyDataGraph = async (req, res) => {
     data &&
       data.forEach((val) => {
         if (val.paymentData !== null) {
-          const date = moment(val.paymentData.create_time).format("MM D YYYY");
+          const date = moment(val.paymentData.create_time).format(
+            "MMMM D YYYY"
+          );
           dailyData[date] = dailyData[date] || {
             responses: [],
             totalAmount: [],
@@ -245,10 +247,10 @@ exports.getDailyDataGraph = async (req, res) => {
 
           // dailyData[date].responses.push(val);
           dailyData[date].totalAmount.push(
-            Math.floor(val.paymentData.transactions[0].amount.total)
+            Math.floor(val.paymentData.transactions[0].amount.total++)
           );
           dailyData[date].dates.push(
-            moment(val.paymentData.create_time).format("MM D YYYY")
+            moment(val.paymentData.create_time).format("MMMM D YYYY")
           );
           dailyData[date].totalSum = dailyData[date].totalAmount.reduce(
             function (a, b) {
