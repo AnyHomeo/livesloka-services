d24eb07b678a4ad4779455531ccbecca18841a0a
E.Karthik <kartheekenumarthi@gmail.com>
2021-01-17 11:51:04 +0530

    fix: salary with number of students consideration

--- controllers/salary.controller.js
+++ controllers/salary.controller.js
@@ -1,4 +1,5 @@
 const Attendance = require("../models/Attendance");
+const CustomerModel = require("../models/Customer.model");
 const TeacherModel = require("../models/Teacher.model");
 
 exports.getAllDatesOfSalaries = async (req, res) => {
@@ -29,69 +30,90 @@ exports.getAllDatesOfSalaries = async (req, res) => {
 exports.getSalariesOfAllTeachersByMonth = async (req, res) => {
   try {
     const { month } = req.query;
+    var finalDataObjectArr = [];
     const allTeachers = await TeacherModel.find({}).lean();
     const allTeacherAttendances = await Attendance.find({
       date: { $regex: month },
-    })
-      .populate("scheduleId", "OneToOne oneToMany className students teacher")
-      .lean();
-    let finalDataObjectArr = [];
-    allTeachers.forEach((teacher) => {
-      let objToPush = {};
-      objToPush.id = teacher.id;
-      objToPush.name = teacher.TeacherName;
-      let allAttendecesTakenByThisTeacher = allTeacherAttendances.filter(
-        (singleAttendance, index) => {
-          return singleAttendance.scheduleId.teacher === teacher.id;
+    }).populate("scheduleId", "OneToOne oneToMany className students teacher");
+    Attendance.populate(
+      allTeacherAttendances,
+      {
+        path: "scheduleId.students",
+        model: CustomerModel,
+        select: "numberOfStudents",
+      },
+      (err, allTeacherAttendances) => {
+        if (err) {
+          console.log(err);
         }
-      );
-      objToPush.classes = allAttendecesTakenByThisTeacher;
-      objToPush.details = {};
-      allAttendecesTakenByThisTeacher.forEach((attendance) => {
-        let className = attendance.scheduleId.className;
-        if (className) {
-          if (!objToPush.details[className]) {
-            objToPush.details[className] = {
-              noOfDays: 1,
-            };
-            if (attendance.scheduleId.OneToOne) {
-              objToPush.details[className].commission =
-                typeof teacher.Commission_Amount_One === "string"
-                  ? parseInt(teacher.Commission_Amount_One)
-                  : 0;
-              objToPush.details[className].totalSalary =
-                typeof teacher.Commission_Amount_One === "string"
-                  ? parseInt(teacher.Commission_Amount_One)
-                  : 0;
-            } else {
-              objToPush.details[className].commission =
-                typeof teacher.Commission_Amount_Many === "string"
-                  ? parseInt(teacher.Commission_Amount_Many)
-                  : 0;
-              objToPush.details[className].totalSalary =
-                typeof teacher.Commission_Amount_Many === "string"
-                  ? parseInt(teacher.Commission_Amount_Many)
-                  : 0;
+        allTeachers.forEach((teacher) => {
+          let objToPush = {};
+          objToPush.id = teacher.id;
+          objToPush.name = teacher.TeacherName;
+          let allAttendecesTakenByThisTeacher = allTeacherAttendances.filter(
+            (singleAttendance, index) => {
+              return singleAttendance.scheduleId.teacher === teacher.id;
             }
-          } else {
-            objToPush.details[className].noOfDays += 1;
-            objToPush.details[className].totalSalary =
-              objToPush.details[className].noOfDays *
-              objToPush.details[className].commission;
-          }
-        }
-      });
-      objToPush.classes = undefined;
-      let totalSalary = 0;
-      Object.keys(objToPush.details).forEach((className) => {
-        totalSalary += objToPush.details[className].totalSalary;
-      });
-      objToPush.totalSalary = totalSalary;
-      finalDataObjectArr.push(objToPush);
-    });
-    return res.json({
-      finalDataObjectArr,
-    });
+          );
+          objToPush.classes = allAttendecesTakenByThisTeacher;
+          objToPush.details = {};
+          allAttendecesTakenByThisTeacher.forEach((attendance) => {
+            let className = attendance.scheduleId.className;
+            let totalStudents = 0;
+            attendance.scheduleId.students.forEach((student) => {
+              totalStudents += student.numberOfStudents
+                ? parseInt(student.numberOfStudents)
+                : 1;
+            });
+            if (className) {
+              if (!objToPush.details[className]) {
+                objToPush.details[className] = {
+                  noOfDays: 1,
+                };
+                if (attendance.scheduleId.OneToOne) {
+                  objToPush.details[className].commission =
+                    typeof teacher.Commission_Amount_One === "string"
+                      ? parseInt(teacher.Commission_Amount_One)
+                      : 0;
+                  objToPush.details[className].totalSalary =
+                    typeof teacher.Commission_Amount_One === "string"
+                      ? parseInt(teacher.Commission_Amount_One)
+                      : 0;
+                } else {
+                  objToPush.details[className].commission =
+                    typeof teacher.Commission_Amount_Many === "string"
+                      ? parseInt(teacher.Commission_Amount_Many)
+                      : 0;
+                  objToPush.details[className].totalSalary =
+                    typeof teacher.Commission_Amount_Many === "string"
+                      ? parseInt(teacher.Commission_Amount_Many)
+                      : 0;
+                }
+                objToPush.details[className].numberOfStudents = totalStudents;
+                objToPush.details[className].totalSalary =
+                  objToPush.details[className].totalSalary * totalStudents;
+              } else {
+                objToPush.details[className].noOfDays += 1;
+                objToPush.details[className].totalSalary =
+                  objToPush.details[className].noOfDays *
+                  objToPush.details[className].numberOfStudents *
+                  objToPush.details[className].commission;
+              }
+            }
+          });
+          objToPush.classes = undefined;
+          let totalSalary = 0;
+          Object.keys(objToPush.details).forEach((className) => {
+            totalSalary += objToPush.details[className].totalSalary;
+          });
+          objToPush.totalSalary = totalSalary;
+          finalDataObjectArr.push(objToPush);
+        });
+        return res.json({
+          finalDataObjectArr,
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
     return res.status(500).json({
