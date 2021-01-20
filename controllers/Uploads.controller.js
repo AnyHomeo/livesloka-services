14846e909dfc52c2741248fd05fe2aa023f3da20
charankurru <61787496+charankurru@users.noreply.github.com>
2021-01-20 18:56:03 +0530

    api semi completed

--- controllers/Uploads.controller.js
+++ controllers/Uploads.controller.js
@@ -7,7 +7,7 @@ const Uploads = require("../models/uploads.model");
 
 exports.GetTeacherSchedules = async (req, res) => {
     let teacherId = req.params.id;
-    let allSchds = await Schedule.find({ id: teacherId })
+    let allSchds = await Schedule.find({ teacher: teacherId })
     console.log(allSchds.length)
     ActiveSchds = allSchds.filter(el => el.isDeleted === false)
     console.log(ActiveSchds.length);
@@ -18,9 +18,19 @@ exports.GetTeacherSchedules = async (req, res) => {
         eachObj['ClassName'] = el.className;
         obj.push(eachObj);
     })
-    console.log(obj)
+    console.log(obj);
+    try {
+        return res.status(200).json({ message: "Teacher Scheduled Fetched", obj });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error });
+    }
+
 }
 
 exports.PostUpload = async (req, res) => {
     console.log(req.body);
 }
+
+
+
