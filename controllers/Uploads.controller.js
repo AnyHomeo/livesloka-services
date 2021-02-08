fcc4afb871de8a436c976e154b58a6604b071461
E.Karthik <kartheekenumarthi@gmail.com>
2021-02-08 18:54:16 +0530

    feat: assign delete view materials

--- controllers/Uploads.controller.js
+++ controllers/Uploads.controller.js
@@ -4,10 +4,13 @@ const Category = require("../models/Category.model");
 const Schedule = require("../models/Scheduler.model");
 const Attendence = require("../models/Attendance");
 const Uploads = require("../models/uploads.model");
+const uploadsModel = require("../models/uploads.model");
 
 exports.GetTeacherSchedules = async (req, res) => {
   let teacherId = req.params.id;
-  let allSchds = await Schedule.find({ teacher: teacherId });
+  let allSchds = await Schedule.find({
+    teacher: teacherId,
+  });
   ActiveSchds = allSchds.filter((el) => el.isDeleted === false);
   let obj = [];
   ActiveSchds.forEach((el) => {
@@ -17,36 +20,29 @@ exports.GetTeacherSchedules = async (req, res) => {
     obj.push(eachObj);
   });
   try {
-    return res.status(200).json({ message: "Teacher Scheduled Fetched", obj });
+    return res.status(200).json({
+      message: "Teacher Scheduled Fetched",
+      obj,
+    });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error });
+    return res.status(500).json({
+      error,
+    });
   }
 };
 
 exports.PostUpload = async (req, res) => {
-  let scheduleid = req.body.scheduleId;
-  req.body.scheduleId = undefined;
   try {
-    let postdata = await Uploads.insertMany(req.body);
-    let scheduleData = await Schedule.find({ _id: scheduleid });
-    let studentsids = scheduleData[0].students;
-    CustomerModel.updateMany(
-      { _id: { $in: studentsids } },
-      {
-        $push: {
-          materials: postdata[0]._id,
-        },
-      }
-    )
-      .then((data) => {})
-      .catch((err) => {
-        console.log(err);
-      });
-    return res.status(200).json({ message: "Material uploaded Successfully" });
+    await Uploads.insertMany(req.body);
+    return res.json({
+      message: "Material Uploaded Successfully !",
+    });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error });
+    return res.status(500).json({
+      error: "Error in uploading Material !",
+    });
   }
 };
 
@@ -56,14 +52,108 @@ exports.GetStudentsMaterial = async (req, res) => {
     let stdmatdata = await CustomerModel.find({
       email: stdId,
     }).populate("materials");
-
     let mat = [];
     stdmatdata.forEach((el) => {
       mat.push(...el.materials);
     });
-    return res.status(200).json({ message: "Fetched Succesfully", mat });
+    return res.status(200).json({
+      message: "Fetched Succesfully",
+      mat,
+    });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error });
+    return res.status(500).json({
+      error,
+    });
   }
+};
+
+exports.assignMaterial = async (req, res) => {
+  let { scheduleId, materialId } = req.body;
+  let scheduleData = await Schedule.find({
+    _id: scheduleId,
+  });
+  let studentsIds = scheduleData[0].students;
+  CustomerModel.updateMany(
+    {
+      _id: {
+        $in: studentsIds,
+      },
+    },
+    {
+      $push: {
+        materials: materialId,
+      },
+    }
+  )
+    .then((data) => {
+      return res.json({
+        message: "Material Assigned Successfully !",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        error: "problem in assigning the material",
+      });
+    });
+};
+
+exports.getMaterialsByTeacherId = async (req, res) => {
+  try {
+    const { teacherId } = req.params;
+    let uploadsByTeacher = await Uploads.find({
+      teacherId,
+    });
+    let uploadIds = uploadsByTeacher.map((upload) => upload._id);
+    let customersWithThisTeacherMaterials = await CustomerModel.find({
+      materials: {
+        $in: uploadIds,
+      },
+    }).select("materials firstName");
+    let finalMaterials = [];
+    uploadsByTeacher.forEach((upload) => {
+      let uploadWithUsers = {
+        ...upload,
+        students: [],
+      };
+      customersWithThisTeacherMaterials.forEach((customer) => {
+        if (customer.materials.includes(upload._id)) {
+          uploadWithUsers.students.push(customer);
+        }
+      });
+      finalMaterials.push(uploadWithUsers);
+    });
+    return res.json({
+      result: finalMaterials,
+      message: "All Materials fetched successfully !",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error in retrieving data!",
+    });
+  }
+};
+
+exports.deleteMaterial = async (req, res) => {
+  const materialId = req.params;
+  await uploadsModel.deleteOne({
+    _id: materialId,
+  });
+  let customersWithThisMaterial = await CustomerModel.find({
+    materials: materialId,
+  }).select("materials");
+  customersWithThisMaterial.forEach(async (customer) => {
+    if (
+      Array.isArray(customer.materials) &&
+      customer.materials.includes(materialId)
+    ) {
+      let index = customer.materials.indexOf(materialId);
+      if (index !== -1) {
+        customer.materials.splice(index, 1);
+      }
+    }
+    await customer.save();
+  });
 };
--- models/uploads.model.js
+++ models/uploads.model.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
 
 var UploadSchema = new mongoose.Schema(
-    {
-        // scheduleId: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: "Schedule",
-        // },
-        className: { type: String },
-        materialName: { type: String },
-        UploadLink: {
-            trim: true,
-            type: String,
-        },
-        teacherId: {
-            type: String,
-        },
-        typeOfmaterial: {
-            type: String,
-        }
+  {
+    materialName: {
+      type: String,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+    UploadLink: {
+      trim: true,
+      type: String,
+    },
+    teacherId: {
+      type: String,
+    },
+    typeOfmaterial: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Upload", UploadSchema);
