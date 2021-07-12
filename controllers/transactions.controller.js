6bd8d19ab25d262b7aa0c29eba3cfdf5c56a1529
karthik enumarthi <kartheekenumarthi@gmail.com>
2021-07-12 17:01:22 +0530

    fix: multiple dates

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -11,42 +11,60 @@ function getDaysInMonth(month, year) {
   return days;
 }
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 exports.getTransactionsData = async (req, res) => {
-  const { month } = req.query;
-  //2021-02 format month
-  let splittedMonth = parseInt(month.split("-")[1]);
-  let splittedYear = parseInt(month.split("-")[0]);
-  let startOfMonth = momentTZ(month, "YYYY-MM")
-    .tz("Asia/Kolkata")
-    .startOf("month")
-    .format();
-  let endOfMonth = momentTZ(month, "YYYY-MM")
-    .tz("Asia/Kolkata")
-    .endOf("month")
-    .format();
-  let allTransactionsOftheMonth = await Transactions.find({
-    date: {
-      $gte: startOfMonth,
-      $lte: endOfMonth,
-    },
-  }).sort({ date: 1 });
-  let allDates = getDaysInMonth(splittedMonth - 1, splittedYear);
-  allTransactionsOftheMonth = allTransactionsOftheMonth.reduce((prev, next) => {
-    let key = momentTZ(next["date"]).format("YYYY-MM-DD");
-    if (!prev[key]) {
-      prev[key] = [];
-    }
-    prev[key].push(next);
-    return prev;
-  }, {});
-  let dateWiseAmount = allDates.map(
-    (date) =>
-    allTransactionsOftheMonth[date] ? allTransactionsOftheMonth[date]
-        .map((transaction) => transaction.amount)
-        .reduce((a, b) => a + b,0) : 0);
-  return res.json({
-    allDates,
-    dateWiseAmount,
-    totalAmount:dateWiseAmount.reduce((a, b) => (parseFloat(a) + parseFloat(b)),0).toFixed(2)
-  });
-};
+  try {
+    const { month } = req.query;
+    //2021-02 format month
+    let months = month.split(",")
+    let output = {}
+    await asyncForEach(months,async (month) => {
+      let splittedMonth = parseInt(month.split("-")[1]);
+      let splittedYear = parseInt(month.split("-")[0]);
+      let startOfMonth = momentTZ(month, "YYYY-MM")
+        .tz("Asia/Kolkata")
+        .startOf("month")
+        .format();
+      let endOfMonth = momentTZ(month, "YYYY-MM")
+        .tz("Asia/Kolkata")
+        .endOf("month")
+        .format();
+      let allTransactionsOftheMonth = await Transactions.find({
+        date: {
+          $gte: startOfMonth,
+          $lte: endOfMonth,
+        },
+      }).sort({ date: 1 });
+      let allDates = getDaysInMonth(splittedMonth - 1, splittedYear);
+      allTransactionsOftheMonth = allTransactionsOftheMonth.reduce((prev, next) => {
+        let key = momentTZ(next["date"]).format("YYYY-MM-DD");
+        if (!prev[key]) {
+          prev[key] = [];
+        }
+        prev[key].push(next);
+        return prev;
+      }, {});
+      let dateWiseAmount = allDates.map(
+        (date) =>
+        allTransactionsOftheMonth[date] ? allTransactionsOftheMonth[date]
+            .map((transaction) => transaction.amount)
+            .reduce((a, b) => a + b,0) : 0);
+      
+      let extraArr = [...new Array(31 - dateWiseAmount.length)].map(_ => 0)
+      output[month] = dateWiseAmount.concat(extraArr)
+    })
+    return res.json({
+      result:output
+    })    
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      error:"Something went wrong!"
+    })
+  }
+    };
