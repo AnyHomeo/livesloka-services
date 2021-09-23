4762544924c48e0795c1c8d2655dd33f0f0ee34d
karthik enumarthi <kartheekenumarthi@gmail.com>
2021-09-23 20:17:01 +0530

    feat: added id

--- models/VideoCategories.model.js
+++ models/VideoCategories.model.js
@@ -6,6 +6,9 @@ const videoCategorySchema = new mongoose.Schema({
         required:true,
         unique:true,
     },
+    id:{
+        type:String
+    }
 });
 
 module.exports = mongoose.model('VideoCategory', videoCategorySchema);
