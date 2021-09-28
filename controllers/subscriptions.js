15ad6b8e769f6177bc23195de56f00a2c513dee3
karthik enumarthi <kartheekenumarthi@gmail.com>
2021-09-28 19:02:22 +0530

    feat: subscription by customerid

--- controllers/subscriptions.js
+++ controllers/subscriptions.js
@@ -1083,7 +1083,7 @@ exports.cancelSubscription = async (req, res) => {
 
 exports.getAllSubscriptions = async (req, res) => {
   try {
-    const allSubscriptions = await SubscriptionModel.find()
+    const allSubscriptions = await SubscriptionModel.find({customerId:req.params.id})
       .populate("customerId", "firstName lastName")
       .lean();
     return res.json({
