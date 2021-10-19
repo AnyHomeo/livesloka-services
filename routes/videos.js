485fb3d87b87770a63cbe8bdc1cbc3d8caea0551
karthik enumarthi <kartheekenumarthi@gmail.com>
2021-10-19 08:31:12 +0530

    feat: bulk videos

--- routes/videos.js
+++ routes/videos.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
     getVideosByCategoryId,
-    getVideosByAssignedToId
+    getVideosByAssignedToId,
+    uploadBulkVideos
 } = require("../controllers/videos");
 
 router.get("/category/:id", getVideosByCategoryId);
 router.get("/customer/:userId", getVideosByAssignedToId);
+router.post("/bulk",uploadBulkVideos)
 
 module.exports = router;
