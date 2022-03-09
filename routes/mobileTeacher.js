2536529abb6414102e9f7f726115bdf7995607f6
karthik <karthikenumarthi@gmail.com>
2022-03-09 10:04:16 +0530

    remove security

--- routes/mobileTeacher.js
+++ routes/mobileTeacher.js
@@ -2,8 +2,19 @@ const express = require("express");
 const { isAdmin, isLoggedId } = require("../controllers/helpers.js");
 const router = express.Router();
 
-const { getTeachersCategoried, getTeacherSchedules } = require("../controllers/mobileTeacher.js");
+const {
+  getTeachersCategoried,
+  getTeacherSchedules,
+} = require("../controllers/mobileTeacher.js");
 
-router.get('/categories',isLoggedId,isAdmin,getTeachersCategoried);
-router.get('/:teacherId/schedules',isLoggedId,isAdmin,getTeacherSchedules);
+router.get(
+  "/categories",
+  //   isLoggedId,isAdmin,
+  getTeachersCategoried
+);
+router.get(
+  "/:teacherId/schedules",
+  //   isLoggedId,isAdmin,
+  getTeacherSchedules
+);
 module.exports = router;
