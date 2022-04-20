87fc248045433fe757092e2a64cf498e443a7de4
karthik <karthikenumarthi@gmail.com>
2022-04-20 09:53:09 +0530

    wip

---
+++ models/Views.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const ViewsSchema = new Schema(
+  {
+    id: {
+      type: String,
+    },
+    query: {
+      type: Schema.Types.Mixed,
+    },
+    sortBy: {
+      type: String,
+    },
+    isAsc: {
+      type: Boolean,
+      default: true,
+    },
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Views", ViewsSchema);
